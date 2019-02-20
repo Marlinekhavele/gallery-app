d4343f5c795fbd22f57cb3d5681a8a85aff7b172
MarlineKhavele <marlinekhavele6@gmail.com>
2019-02-20 15:04:49 +0300

    Updates

---
+++ precache-manifest.9d474bd1f8645cad2f55f2b3203eaf42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2a4afa2585e5cfc6ac8",
+    "url": "/gallery-api/static/css/main.000821ac.chunk.css"
+  },
+  {
+    "revision": "e2a4afa2585e5cfc6ac8",
+    "url": "/gallery-api/static/js/main.e2a4afa2.chunk.js"
+  },
+  {
+    "revision": "66db97f37ace12a258d4",
+    "url": "/gallery-api/static/js/runtime~main.66db97f3.js"
+  },
+  {
+    "revision": "eb33f06dc1cfe1ec4c75",
+    "url": "/gallery-api/static/js/2.eb33f06d.chunk.js"
+  },
+  {
+    "revision": "fceb7e47e10731bb276385b0155a2e03",
+    "url": "/gallery-api/index.html"
+  }
+];
