fee9a76d0b7bef8e2b5c48ad870bc2fc89ec2d4c
MarlineKhavele <marlinekhavele6@gmail.com>
2019-02-20 10:33:16 +0300

    added the client id

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
 import './App.css';
+import ImgList from './Components/ImgList';
+import SearchForm from './Components/SearchForm';
+import cred from './cred.js';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export default class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			imgs: [],
+			loadingState: true
+		};
+	}
+
+	componentDidMount() {
+		this.performSearch();
+	}
+
+	performSearch = (query = 'sun') => {
+		axios
+			.get(
+				`https://api.unsplash.com/search/photos/?page=1&per_page=10&query=${query}&client_id=67148e3ee5523b5c4637cbba6d317bdd39d16055a114b1b4700bc85d43b5b9fd`
+			)
+			.then(data => {
+				this.setState({ imgs: data.data.results, loadingState: false });
+			})
+			.catch(err => {
+				console.log('Error happened during fetching!', err);
+			});
+	};
+
+	render() {
+		return (
+			<div>
+				<div className="main-header">
+					<div className="inner">
+						<h1 className="main-title">ImageSearch</h1>
+						<SearchForm onSearch={this.performSearch} />
+					</div>
+				</div>
+				<div className="main-content">
+					{this.state.loadingState
+						? <p>Loading</p>
+						: <ImgList data={this.state.imgs} />}
+				</div>
+			</div>
+		);
+	}
 }
-
-export default App;
